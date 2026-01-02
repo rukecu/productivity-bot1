a113a11f9b036e288da4bddb7919f2b8a21c0588
rukecu <daniil.poliakov.03@gmail.com>
2026-01-02 23:49:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Запускаем бота в фоне и веб-сервер на переднем плане
-python bot.py &
-gunicorn web_app:app --bind 0.0.0.0:$PORT
+python bot.py & gunicorn web_app:app --bind 0.0.0.0:$PORT
